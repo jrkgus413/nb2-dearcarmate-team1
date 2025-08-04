1c21087729c038032afb2e28e7475dfd7738acd8
jrkgus413 <jrkgus413@naver.com>
2025-08-04 18:12:06 +0900

    fix: 회사 라우터에 관리자 권한 미들웨어 추가

--- src/routes/company.router.ts
+++ src/routes/company.router.ts
@@ -1,17 +1,19 @@
 import express from 'express';
+import { allow } from '../middlewares/allow.middleware';
+import { USER_ROLE } from '../enums/user.enum';
 import * as CompanyController from '../controllers/company.controller';
 
 const router = express.Router();
 
 // 회사 생성
-router.route('/').post(CompanyController.createCompany);
+router.route('/').post(allow([USER_ROLE.ADMIN]), CompanyController.createCompany);
 // 회사 목록 조회
-router.route('/').get(CompanyController.getCompanies);
+router.route('/').get(allow([USER_ROLE.ADMIN]), CompanyController.getCompanies);
 // 회사별 유저 조회
-router.route('/users').get(CompanyController.getUserbyCompany);
+router.route('/users').get(allow([USER_ROLE.ADMIN]), CompanyController.getUserbyCompany);
 // 회사 정보 수정
-router.route('/:companyId').patch(CompanyController.updateCompanies);
+router.route('/:companyId').patch(allow([USER_ROLE.ADMIN]), CompanyController.updateCompanies);
 // 회사 정보 삭제
-router.route('/:companyId').delete(CompanyController.deleteCompanies);
+router.route('/:companyId').delete(allow([USER_ROLE.ADMIN]), CompanyController.deleteCompanies);
 
 export default router;
