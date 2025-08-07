5c37aeed7265a1d39fcff8bfb01b2a7dc5d0946d
gyunam-bark <gyunam.bark@gmail.com>
2025-08-07 23:51:30 +0900

    fix: 계약서 다운로드 프론트 수정에 맞춰서 반환값 수정

--- src/handlers/global-error.handler.ts
+++ src/handlers/global-error.handler.ts
@@ -1,5 +1,10 @@
 import { ErrorRequestHandler } from 'express';
-import { isErrorInstanceOfHttp, isErrorInstanceOfMulter, isErrorInstanceOfNode } from '../utils/error.util';
+import {
+  isErrorInstanceOfHttp,
+  isErrorInstanceOfJwt,
+  isErrorInstanceOfMulter,
+  isErrorInstanceOfNode,
+} from '../utils/error.util';
 import { getIp, getMethod, getUrl } from '../utils/from.util';
 
 export const globalErrorHandler: ErrorRequestHandler = async (error, req, res, _next) => {
@@ -9,12 +14,15 @@ export const globalErrorHandler: ErrorRequestHandler = async (error, req, res, _
   if (isErrorInstanceOfHttp(error)) {
     status = error.status;
     message = error.message;
-  } else if (isErrorInstanceOfNode(error)) {
-    status = 500;
-    message = error.message;
   } else if (isErrorInstanceOfMulter(error)) {
     status = 400;
     message = error.message;
+  } else if (isErrorInstanceOfJwt(error)) {
+    status = 401;
+    message = error.message;
+  } else if (isErrorInstanceOfNode(error)) {
+    status = 500;
+    // message = error.message;
   }
 
   const log = {
